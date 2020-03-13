a232499df5937fb59b02912d10c16bbdd503658b
labrosb <labros_b@hotmail.com>
2020-03-13 01:52:14 +0100

    Back End implemented

---
+++ Back-End/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import data from './datasets/phones';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
+
+app.use('/images', express.static(__dirname + '/images'));
+
+app.get('/phones', (req, res) => {
+  if (data.length === 0) {
+    res.status(400).send("No Phones found!");
+  }
+  res.status(200).send(data);
+});
+
+app.listen(8080, () => {
+  console.log("Server started on port 8080");
+});
